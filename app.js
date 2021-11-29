b8da90a4b0bc63c627f67e05935bbce22db6bd81
Georgi <georgi.nenov89@gmail.com>
2021-11-29 15:45:49 -0500

    UI and Item controllers  added

--- app.js
+++ app.js
@@ -20,8 +20,11 @@ const ItemCtrl = (function(){
       currentItem: null,
       totalCalories: 0
     }
-    // ublic method
+    // Public method
     return {
+      getItems: function(){
+        return data.items;
+      }, 
       logData: function(){
         return data;
       }
@@ -30,7 +33,26 @@ const ItemCtrl = (function(){
 
 // UI Controller
 const UICtrl = (function(){
+  const UISelectors = {
+    itemList: '#item-list'
+  }
+  // Public methods 
+  return {
+  populateItemList: function(items){
+    let html = '';
+    items.forEach(function(item){
+      html += `<li class="collection-item" id="item-${item.id}">
+      <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+      <a href="#" class="secondary-content">
+        <i class="edit-item fa fa-pencil"></i>
+      </a>
+    </li>`
+    });
 
+    // Insert List Items
+    document.querySelector(UISelectors.itemList).innerHTML = html;
+  }
+  }
 })();
 
 // App Controller
@@ -39,7 +61,12 @@ const App = (function(ItemCtrl, UICtrl){
   // Public methods
   return {
     init: function(){
-      console.log('Initializing App...')
+      console.log('Initializing App...');
+      const items = ItemCtrl.getItems();
+      // console.log(items); 
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
 })(ItemCtrl, UICtrl);
